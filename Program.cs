a5efa42987649cf2c77ed352759756f01a05ad4c
Lukas Selin <lukas.selin@viskan.se>
2019-09-20 11:27:48 +0200

    uppgift 1

--- Program.cs
+++ Program.cs
@@ -6,7 +6,25 @@ namespace vektorer
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] five_int_array = new int[5];
+
+            for (int i = 0; i < 5; i++){
+                Console.WriteLine("Skriv in ett tal: ");
+
+                string input = Console.ReadLine();
+                int tal_input = Convert.ToInt32(input);
+
+                five_int_array[i] = tal_input;
+            }
+            
+            int sum = 0;
+            for (int i = 0; i < 5; i++){
+                int foobar = five_int_array[i];
+                sum += foobar;
+            }
+
+            Console.WriteLine("Din summa blev: {0}", sum);
+
         }
     }
 }
